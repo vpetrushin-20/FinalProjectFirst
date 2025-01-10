e2101ea2d3d8efa6efe5b1f34f4c5d529bcc0a2e
kxkxk <kxkxkxldl@mail.ru>
2025-01-10 15:02:49 +0300

    Без параллельного запуска и без мобилки

--- src/test/java/mobile/BaseTestMobile.java
+++ src/test/java/mobile/BaseTestMobile.java
@@ -1,37 +1,37 @@
-package mobile;
-
-import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.Selenide;
-import com.codeborne.selenide.logevents.SelenideLogger;
-import io.github.bonigarcia.wdm.WebDriverManager;
-import io.qameta.allure.selenide.AllureSelenide;
-import org.junit.After;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
-import org.openqa.selenium.chrome.ChromeOptions;
-
-
-public abstract class BaseTestMobile {
-
-    @BeforeAll
-    public static void setUp() {
-        WebDriverManager.chromedriver().setup();
-        Configuration.browser = "chrome";
-        Configuration.webdriverLogsEnabled = true;
-        Configuration.headless = true;
-        Configuration.browserSize = "375x812";
-
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("user-agent=Mozilla/5.0 (iPhone; CPU iPhone OS 14_0 like Mac OS X) AppleWebKit/605.1.1 (KHTML, like Gecko) Mobile/15E148");
-
-        Configuration.browserCapabilities = options;
-        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
-        System.setProperty("chromeoptions.args", "\"--no-sandbox\",\"--disable-dev-shm-usage\",\"--remote-debugging-port=9222\",\"--headless\"");
-    }
-
-    @After
-    public void tearDown() {
-        Selenide.closeWebDriver();
-    }
-
-}
+//package mobile;
+//
+//import com.codeborne.selenide.Configuration;
+//import com.codeborne.selenide.Selenide;
+//import com.codeborne.selenide.logevents.SelenideLogger;
+//import io.github.bonigarcia.wdm.WebDriverManager;
+//import io.qameta.allure.selenide.AllureSelenide;
+//import org.junit.After;
+//import org.junit.jupiter.api.AfterAll;
+//import org.junit.jupiter.api.BeforeAll;
+//import org.openqa.selenium.chrome.ChromeOptions;
+//
+//
+//public abstract class BaseTestMobile {
+//
+//    @BeforeAll
+//    public static void setUp() {
+//        WebDriverManager.chromedriver().setup();
+//        Configuration.browser = "chrome";
+//        Configuration.webdriverLogsEnabled = true;
+//        Configuration.headless = true;
+//        Configuration.browserSize = "375x812";
+//
+//        ChromeOptions options = new ChromeOptions();
+//        options.addArguments("user-agent=Mozilla/5.0 (iPhone; CPU iPhone OS 14_0 like Mac OS X) AppleWebKit/605.1.1 (KHTML, like Gecko) Mobile/15E148");
+//
+//        Configuration.browserCapabilities = options;
+//        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+//        System.setProperty("chromeoptions.args", "\"--no-sandbox\",\"--disable-dev-shm-usage\",\"--remote-debugging-port=9222\",\"--headless\"");
+//    }
+//
+//    @After
+//    public void tearDown() {
+//        Selenide.closeWebDriver();
+//    }
+//
+//}
--- src/test/java/mobile/MobileWebTest.java
+++ src/test/java/mobile/MobileWebTest.java
@@ -1,66 +1,66 @@
-package mobile;
-
-import com.codeborne.selenide.Selenide;
-import io.qameta.allure.*;
-import mobilePages.MobileDebetCardMtsMoneyPage;
-import mobilePages.MobileMainPage;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-
-import java.util.Random;
-
-import static com.codeborne.selenide.Selenide.open;
-
-@Epic("Тесты Mobile")
-@Link(name = "Сайт мтс банка", url = "https://www.mtsbank.ru/")
-@Owner("Петрушин Вадим Васильевич")
-public class MobileWebTest extends BaseTestMobile {
-
-    final static String FIO_ERROR = "Сергей1";
-    final static String EMAIL_ERROR = "uka1980bk.ru";
-
-
-    @Test
-    @Description("Проверка на заполнение полей формы дебетовой карты МТС деньги с ошибками и проверка сообщений об ошибках")
-    @DisplayName("Проверка на заполнение формы с ошибками")
-    @Severity(SeverityLevel.TRIVIAL)
-    public void checkErrorForm() {
-        open("https://www.mtsbank.ru/");
-        MobileMainPage mobileMainPage = new MobileMainPage();
-        mobileMainPage.clickMobileButtonDebetCard();
-
-        Random rn = new Random();
-        int minimum = 900000000;
-        int maximum = 999999999;
-        int range = maximum - minimum + 1;
-        int randomNum = rn.nextInt(range) + minimum;
-        String randomNumberAsString = String.valueOf(randomNum);
-
-        MobileDebetCardMtsMoneyPage mobileDebetCardMtsMoneyPage = new MobileDebetCardMtsMoneyPage();
-        mobileDebetCardMtsMoneyPage.pngClick()
-                .enterErrorFormPhone(randomNumberAsString)
-                .enterErrorFormFio(FIO_ERROR)
-                .enterErrorFormEmail(EMAIL_ERROR)
-                .enterErrorFormFio(FIO_ERROR)
-                .mobileCheckingFields();
-
-        Selenide.closeWebDriver();
-        Selenide.clearBrowserCookies();
-    }
-
-    @Test
-    @Description("Проверка на наличие только дебетовых карт в списке при переходе на вкладку 'дебетовые")
-    @DisplayName("Проверка на отображение ожидаемых элементов")
-    @Severity(SeverityLevel.TRIVIAL)
-    public void checkListCardHeaders() {
-        open("https://www.mtsbank.ru/"); // Замените на ваш URL
-        MobileMainPage mobileMainPage = new MobileMainPage();
-        mobileMainPage.clickMobileButtonDebetCard();
-
-        MobileDebetCardMtsMoneyPage mobileDebetCardMtsMoneyPage = new MobileDebetCardMtsMoneyPage();
-        mobileDebetCardMtsMoneyPage.mobileCheckDebetHeaders();
-
-        Selenide.closeWebDriver();
-        Selenide.clearBrowserCookies();
-    }
-}
+//package mobile;
+//
+//import com.codeborne.selenide.Selenide;
+//import io.qameta.allure.*;
+//import mobilePages.MobileDebetCardMtsMoneyPage;
+//import mobilePages.MobileMainPage;
+//import org.junit.jupiter.api.DisplayName;
+//import org.junit.jupiter.api.Test;
+//
+//import java.util.Random;
+//
+//import static com.codeborne.selenide.Selenide.open;
+//
+//@Epic("Тесты Mobile")
+//@Link(name = "Сайт мтс банка", url = "https://www.mtsbank.ru/")
+//@Owner("Петрушин Вадим Васильевич")
+//public class MobileWebTest extends BaseTestMobile {
+//
+//    final static String FIO_ERROR = "Сергей1";
+//    final static String EMAIL_ERROR = "uka1980bk.ru";
+//
+//
+//    @Test
+//    @Description("Проверка на заполнение полей формы дебетовой карты МТС деньги с ошибками и проверка сообщений об ошибках")
+//    @DisplayName("Проверка на заполнение формы с ошибками")
+//    @Severity(SeverityLevel.TRIVIAL)
+//    public void checkErrorForm() {
+//        open("https://www.mtsbank.ru/");
+//        MobileMainPage mobileMainPage = new MobileMainPage();
+//        mobileMainPage.clickMobileButtonDebetCard();
+//
+//        Random rn = new Random();
+//        int minimum = 900000000;
+//        int maximum = 999999999;
+//        int range = maximum - minimum + 1;
+//        int randomNum = rn.nextInt(range) + minimum;
+//        String randomNumberAsString = String.valueOf(randomNum);
+//
+//        MobileDebetCardMtsMoneyPage mobileDebetCardMtsMoneyPage = new MobileDebetCardMtsMoneyPage();
+//        mobileDebetCardMtsMoneyPage.pngClick()
+//                .enterErrorFormPhone(randomNumberAsString)
+//                .enterErrorFormFio(FIO_ERROR)
+//                .enterErrorFormEmail(EMAIL_ERROR)
+//                .enterErrorFormFio(FIO_ERROR)
+//                .mobileCheckingFields();
+//
+//        Selenide.closeWebDriver();
+//        Selenide.clearBrowserCookies();
+//    }
+//
+//    @Test
+//    @Description("Проверка на наличие только дебетовых карт в списке при переходе на вкладку 'дебетовые")
+//    @DisplayName("Проверка на отображение ожидаемых элементов")
+//    @Severity(SeverityLevel.TRIVIAL)
+//    public void checkListCardHeaders() {
+//        open("https://www.mtsbank.ru/"); // Замените на ваш URL
+//        MobileMainPage mobileMainPage = new MobileMainPage();
+//        mobileMainPage.clickMobileButtonDebetCard();
+//
+//        MobileDebetCardMtsMoneyPage mobileDebetCardMtsMoneyPage = new MobileDebetCardMtsMoneyPage();
+//        mobileDebetCardMtsMoneyPage.mobileCheckDebetHeaders();
+//
+//        Selenide.closeWebDriver();
+//        Selenide.clearBrowserCookies();
+//    }
+//}
